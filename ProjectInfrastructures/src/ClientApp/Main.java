ba9d9b8e1c04f63afd45be356d7ee7ca025c68ee
Wisse Voortman <w_voortman@hotmail.com>
2017-12-08 09:55:06 +0100

    Client App - initital commit

---
+++ ProjectInfrastructures/src/ClientApp/Main.java
@@ -0,0 +1,11 @@
+package ClientApp;
+
+public class Main {
+	private static final String version = "1.0";
+	
+
+	public static void methodname(){
+		System.out.println("Je moet Gewoon lezen.......");
+	}
+
+}
---
+++ ProjectInfrastructures/src/ClientApp/Runner.java
@@ -0,0 +1,9 @@
+package ClientApp;
+
+public class Runner {
+    public static void main(String[] args) {
+    	new Main();
+    	Main.methodname();
+    }
+    
+}
