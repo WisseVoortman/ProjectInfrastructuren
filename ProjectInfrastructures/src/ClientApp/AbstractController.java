5b15626ccb2e8de34e58c1a0bd71dbf67b284b3f
Wisse Voortman <w_voortman@hotmail.com>
2017-12-08 14:00:17 +0100

    Committing the Client application BareBones. Based on MVC design pattern.

---
+++ ProjectInfrastructures/src/ClientApp/AbstractController.java
@@ -0,0 +1,16 @@
+package ClientApp;
+
+import javax.swing.*;
+
+import java.awt.event.*;
+
+@SuppressWarnings("serial")
+public abstract class AbstractController extends JPanel implements ActionListener {
+
+	protected Model model;
+	
+	public AbstractController(Model model) {
+		this.model=model;
+	}
+	
+}
--- ProjectInfrastructures/src/ClientApp/Model.java
+++ ProjectInfrastructures/src/ClientApp/Model.java
@@ -1,1 +1,50 @@
 package ClientApp;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+
+
+/*
+ * the data and the methods to work with it.
+ */
+public class Model implements Runnable{
+
+	private List<AbstractView> views;
+	private int var = 0;
+	
+    public Model() {
+    	//constructor
+    }
+    
+    public void start() {
+    	// automaticaly calls run();
+		new Thread(this).start();
+	}
+    
+    public void run() {
+    	//initial given run method based on a number of ticks
+    }
+    
+    public void stop(){
+    	
+    }
+    
+    public void addView(AbstractView view) {
+		views.add(view);
+	}
+
+    public void updateViews(){
+    	
+        for( AbstractView v : views ) {
+           // v.updateView();
+            v.repaint();
+        }
+    }
+    
+    public List<AbstractView> getViews() {
+		return views;
+	};
+}
+
