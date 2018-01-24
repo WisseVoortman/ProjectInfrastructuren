b2e6710dc8bcffcfbd7884104de9891439ec40de
woesterudolf <rudolfklijnhout@hotmail.com>
2018-01-24 14:03:24 +0100

    oplossing?

--- ProjectInfrastructures/public html/index.php
+++ ProjectInfrastructures/public html/index.php
@@ -14,21 +14,16 @@
 include '/functions/head.php';
 ?>
 <body>
-<<<<<<< HEAD
+	<header>
+	</header>
+	<!--<div class="row">-->
+	<div class="container">
+	</div>
 	<h1>Please login to continue</h1>
 	<?php if(!empty($error))  echo '<p color="red">'. $error .'</p>';    ?>
 	<form action="index.php" method="post">
-=======
-	<!--<div class="row">-->
-	<header>
-	</header>
-	<div class="container">
-	</div>
-	<div class="container">
-		<h1>Please login to continue</h1>
-	</div>
+	
 	<form action="" method="post">
->>>>>>> 291276ce7ec61f36dce98fd66aae866563362667
 
 	  <div class="container">
 	    <label><b>Username</b></label>
