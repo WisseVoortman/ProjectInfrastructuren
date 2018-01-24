c770107b8fb2144b345719e670c82d6da4dd674b
woesterudolf <rudolfklijnhout@hotmail.com>
2018-01-24 15:52:44 +0100

    table added
    selector table added

--- ProjectInfrastructures/public html/dashboard.php
+++ ProjectInfrastructures/public html/dashboard.php
@@ -15,6 +15,35 @@
 		</nav>
 		<div class="container">
 			<h1>Is dit een teletubbie?</h1>
+			
+			<div class="selector">
+				<form action="/dashboard.php">
+					<table class="selector-table">
+						<tr></tr>
+						<tr>				
+							<th>startdate:</th>
+							<td><input type="date" name="startdate"></td>
+						</tr>
+						<tr>
+							<th>enddate:</th>
+							<td><input type="date" name="enddate"></td>
+						</tr>
+						<tr>	
+							<th>starttime:</th>
+							<td><input type="time" name="starttime"></td>
+						</tr>
+						<tr>
+							<th>enddate:</th>
+							<td><input type="time" name="endtime"></td>
+						</tr>
+						<tr>
+							<th colspan="2"><button type="submit">Send</button></th>
+						</tr>
+					</table>
+				</form>
+			</div>
 		</div>
+		<footer>
+		</footer>
 	</body>
 </html>
