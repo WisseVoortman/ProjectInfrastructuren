93c2ec1cdccdea1b6b0555a1a648f37192b58b7a
Wisse Voortman <w_voortman@hotmail.com>
2018-01-24 17:56:59 +0100

    moved some code

--- ProjectInfrastructures/src/ServerApp/GeneralBuffer.java
+++ ProjectInfrastructures/src/ServerApp/GeneralBuffer.java
@@ -13,20 +13,23 @@ public class GeneralBuffer {
 		id = 1;
 		this.map = new HashMap<>();
 		
-		for(int i=id;i<8000;i++){
+		for(int i=id;i<=8000;i++){
 			if (this.map.get(i) == null) { //gets the value for an id)
 			    this.map.put(i, new StationBuffer()); //no StationBuffer assigned, creating a new StationBuffer
 			    System.out.println("created a new StationBuffer for: station " + i + ".");
-			    } // end of if
+			    } // end of for
 		
 		
-		    //System.out.print(this.map.get(id));
-		    //((StationBuffer) this.map.get(id)).testone();    
-		    //((StationBuffer) this.map.get(id)).printqueue();
-		    //((StationBuffer) this.map.get(id)).addToQueue();
-		    //((StationBuffer) this.map.get(id)).printqueue();
+		    
 		
 		} // end of for
+		
+		//System.out.print(this.map.get(id));
+		
+		//((StationBuffer) this.map.get(id)).testone();    
+	    //((StationBuffer) this.map.get(id)).printqueue();
+	    //((StationBuffer) this.map.get(id)).addToQueue();
+	    //((StationBuffer) this.map.get(id)).printqueue();
 		
 	} // end of constructor
 	
