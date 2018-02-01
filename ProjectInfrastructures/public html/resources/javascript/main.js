0a202c81f358bc0ffcea6044ff189e312fb7ced0
woesterudolf <rudolfklijnhout@hotmail.com>
2018-02-01 12:20:02 +0100

    knoppen iets korter

--- ProjectInfrastructures/public html/resources/javascript/main.js
+++ ProjectInfrastructures/public html/resources/javascript/main.js
@@ -1,6 +1,7 @@
 var temphtml = '<div class="dashboardItem" id="tempGauge"></div>';
 var downfallhtml = '<div class="dashboardItem" id="downfallGraph"></div>';
 var customhtml = '<div class="selector" id="custom_selector"><form action="#" onsubmit="return false;"><table class="selector-table"><tr></tr><tr><th>start date and time:</th><td><input type="date" id="startDate"></td><td><input type="time" id="startName"></td></tr><tr><th>end date and time:</th><td><input type="date" id="endDate"></td><td><input type="time" id="endTime"></td></tr><tr><th colspan="3"><button type="submit">Send</button></th></tr></table></form></div>';
+var allhtml = temphtml + "" + downfallhtml;  
   function check_id_not_null() {
     if(document.getElementById("dashboard-items") !== null) {
       return true;
@@ -9,7 +10,7 @@ var customhtml = '<div class="selector" id="custom_selector"><form action="#" on
   }
   function dashboardPage() {
     if(check_id_not_null()) {
-      var allhtml = temphtml + "" + downfallhtml;
+      
       document.getElementById("dashboard-items").innerHTML = allhtml;
 	  downfallGraph();
 	  tempGauge();
@@ -34,7 +35,6 @@ var customhtml = '<div class="selector" id="custom_selector"><form action="#" on
   }
   function loadPage(){
 		if(check_id_not_null()) {
-			var allhtml = temphtml + "" + downfallhtml;
 			document.getElementById("dashboard-items").innerHTML = allhtml;
 			downfallGraph();
 			tempGauge();
