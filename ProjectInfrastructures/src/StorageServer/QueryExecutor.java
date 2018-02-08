09aa930544f302a3dbd0526f378f2c2e0fc1d7a1
root <root@example.com>
2018-02-08 10:03:50 +0100

    New query rev

--- ProjectInfrastructures/src/StorageServer/QueryExecutor.java
+++ ProjectInfrastructures/src/StorageServer/QueryExecutor.java
@@ -1,39 +1,53 @@
 package StorageServer;
 
 import java.io.File;
+import java.io.PrintWriter;
 import java.io.RandomAccessFile;
 import java.text.Format;
 import java.text.SimpleDateFormat;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 //TODO: Documentation
 @SuppressWarnings("Duplicates")
-public class QueryExecutorSingle{
+public class QueryExecutor {
     private ServerMain model;
     private QueryClient client;
     private String stationNumber;
     private String[] query;
+    private PrintWriter out;
 
-    public QueryExecutorSingle(ServerMain model, QueryClient client, String stationnumber, String[] query) {
+    public QueryExecutor(ServerMain model, QueryClient client, String stationnumber, String[] query, PrintWriter out) {
         this.model = model;
         this.client = client;
         this.stationNumber = stationnumber;
         this.query = query;
+        this.out = out;
     }
 
     public void executeQuery() {
         try {
-            /*
-            Send the QueriedData as soon as it has been collected
-             */
-            //TODO: Verify we actually have 2 timestamps for our between
-            //TODO: Get per clause
+            int timeFrom = Integer.parseInt(query[6]);
+            int timeTo = Integer.parseInt(query[8]);
+            byte[] Short = new byte[2];
+            byte[] Int = new byte[4];
+            byte[] Byte = new byte[1];
+
+            // Get DateObjects from time strings
+            Date dateFrom = new Date(timeFrom * 1000L);
+            Date dateTo = new Date(timeTo * 1000L);
+
+            // Generate filenames based on dates
+
+
+            // Get a list of all necessary files
 
         }catch(Exception e) {
             e.printStackTrace();
