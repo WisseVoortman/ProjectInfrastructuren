ce8066655eb0c918ee0a6fd91fe1be1753834585
root <root@example.com>
2018-02-08 14:32:50 +0100

    Final(?+1) Revision Storage Server

--- ProjectInfrastructures/src/StorageServer/ServerRunner.java
+++ ProjectInfrastructures/src/StorageServer/ServerRunner.java
@@ -1,8 +1,8 @@
 package StorageServer;
 
 public class ServerRunner {
-    public static void main(String[] args) { // /home/peasant/Storage/
-       ServerMain server = new ServerMain(30011, 30022, "c:/temp/proj/");
+    public static void main(String[] args) { // /home/peasant/Storage/  c:/temp/proj/
+       ServerMain server = new ServerMain(30011, 30022, "/home/peasant/Storage");
        server.run();
     }
 }
